9530e32eeb926fd0921e42675be8f04187938910
ArunFoeHammer <juanv.blanco@gmail.com>
2026-04-23 18:17:37 +0200

    Fix icon path

--- sw.js
+++ sw.js
@@ -3,5 +3,5 @@ self-invalidate
 onpush:
     self.registration.showNotification('SN Update', {
         body: 'A new push event was enough!',
-        icon: '/icon.png'
+        icon: '/icons/192/pwa-192x192.png'
     });
