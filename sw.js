7897665cc1257992323a2c8ea89f590a20417508
ArunFoeHammer <juanv.blanco@gmail.com>
2026-04-23 19:34:35 +0200

    refactor: extract firebaseConfig to shared firebase-config.js for BTW-33

--- sw.js
+++ sw.js
@@ -1,16 +1,8 @@
 importScripts('https://www.gstatic.com/firebasejs/12.12.1/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/12.12.1/firebase-messaging-compat.js');
+importScripts('firebase-config.js');
 
-// TODO: Replace with actual Firebase config from project settings
-const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_PROJECT_ID.appspot.com",
-    messagingSenderId: "YOUR_SENDER_ID",
-    appId: "YOUR_APP_ID"
-};
-
+// Initialize Firebase using the shared config from firebase-config.js
 firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging();
 
