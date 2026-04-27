9d68d66dbcca116b740cfef59ae46a1b5e49f175
ArunFoeHammer <juanv.blanco@gmail.com>
2026-04-27 16:19:22 +0100

    fix(config): revert accidental deletion of character in apiKey

--- firebase-config.js
+++ firebase-config.js
@@ -1,6 +1,6 @@
 // Replace with Firebase config from project settings
 const firebaseConfig = {
-    apiKey: "AIzaSyAcQom5uTvWvKoxS0nLYOD-lHKc95DjIM",
+    apiKey: "AIzaSyAcQwom5uTvWvKoxS0nLYOD-lHKc95DjIM",
     projectId: "btw-self-notify",
     messagingSenderId: "397570103872",
     appId: "1:397570103872:web:754f5ebc23490c0c2e9da5",
