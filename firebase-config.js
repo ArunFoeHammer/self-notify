6ae94633c205d22865c0648f8176a825de1a58cf
ArunFoeHammer <juanv.blanco@gmail.com>
2026-05-07 20:39:51 +0200

    fix: restore correct vapidKey in firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -4,6 +4,6 @@ const firebaseConfig = {
     projectId: "btw-self-notify",
     messagingSenderId: "397570103872",
     appId: "1:397570103872:web:754f5ebc23490c0c2e9da5",
-    vapidKey: 'BJqlW8icB_5fJ7PMDWax2kVZ_FOdBd5NEBnYNo-r1fmufCG6YZIOiKoMF66RznkvGnNecJZW-1DE6V_7s4m',
+    vapidKey: 'BJqlW8icB5fJ7PMDWax2kVZ_FOdBd5NEBnYNo-r1fmufCG6YZIOiKoMF66RznkvGnNecJZW-1DE6V_7D3Dc-Z2g',
     swVersion: '0.1.5'
 };
